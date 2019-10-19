f678f9d75728df7b3fe4bde8aa5dd5c4954b0071
Ciaran Kelly <ciaran.kelly@gmail.com>
2019-10-19 01:55:46 +0100

    install test

--- install-lab.sh
+++ install-lab.sh
@@ -3,3 +3,17 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+sudo apt-get update && sudo apt-get install docker.io -y
+sudo gpasswd -a ubuntu docker
+newgrp docker << EOF
+minikube start \
+  --kubernetes-version=v1.12.0 \
+  --vm-driver=none \
+  --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+EOF
+
+
+
+
