1c606df4ff61c3eb746c7fb7f7aef8bb2447043b
Ciaran Kelly <ciaran.kelly@gmail.com>
2019-10-22 01:31:41 +0100

    update lab install script

--- install-lab.sh
+++ install-lab.sh
@@ -1,31 +1,69 @@
 #!/usr/bin/env bash
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+install_docker() {
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io socat
+    sudo gpasswd -a ubuntu docker
+}
 
-sudo apt-get update && sudo apt-get install docker.io -y
-sudo gpasswd -a ubuntu docker
+install_kubectl() {
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin
+}
+
+install_minikube() {
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    chmod +x minikube
+    sudo mv minikube /usr/local/bin/
+}
+
+install_helm() {
+    wget https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz
+    tar xvzf helm-v2.14.3-linux-amd64.tar.gz
+    sudo mv linux-amd64/helm /usr/local/bin/
+    rm -rf linux-amd64
+}
+
+install_istio() {
+    export ISTIO_VERSION=1.2.7
+    curl -L https://git.io/getLatestIstio | sh -
+    cd /home/ubuntu/istio-${ISTIO_VERSION}
+    sudo mv ./bin/istioctl /usr/local/bin
+}
+
+install_go() {
+    wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
+    sudo tar -xvf go1.13.1.linux-amd64.tar.gz
+    sudo mv go /usr/local
+}
+
+start_minikube() {
+    sudo minikube start \
+      --kubernetes-version=v1.12.0 \
+      --vm-driver=none \
+      --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+    sudo chown -R ubuntu /home/ubuntu/.kube /home/ubuntu/.minikube
+    helm init
+}
+install_docker
+install_kubectl
+install_minikube
+install_helm
+install_istio
+install_go
+start_minikube
+
+
+deploy() {
 newgrp docker << EOF
-sudo minikube start \
-  --kubernetes-version=v1.12.0 \
-  --vm-driver=none \
-  --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
-
-sudo chown -R $USER $HOME/.kube $HOME/.minikube
-
-wget https://get.helm.sh/helm-v2.15.0-linux-amd64.tar.gz
-tar xvzf helm-v2.15.0-linux-amd64.tar.gz
-sudo mv linux-amd64/helm /usr/local/bin/
-rm -rf linux-amd64
-
-helm init
-
-export ISTIO_VERSION=1.1.7
-curl -L https://git.io/getLatestIstio | sh -
-cd istio-${ISTIO_VERSION}
-for i in install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
+export ISTIO_VERSION=1.2.7
+cd /home/ubuntu/istio-${ISTIO_VERSION}
+for i in install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f ${i}; done
 cat <<ENDISTIO | kubectl apply -f -
 apiVersion: v1
 kind: Namespace
@@ -63,7 +101,38 @@ helm template --namespace=istio-system \
 
 kubectl apply -f istio-lean.yaml
 
+# Add the extra gateway.
+helm template --namespace=istio-system \
+  --set gateways.custom-gateway.autoscaleMin=1 \
+  --set gateways.custom-gateway.autoscaleMax=1 \
+  --set gateways.custom-gateway.cpu.targetAverageUtilization=60 \
+  --set gateways.custom-gateway.labels.app='cluster-local-gateway' \
+  --set gateways.custom-gateway.labels.istio='cluster-local-gateway' \
+  --set gateways.custom-gateway.type='ClusterIP' \
+  --set gateways.istio-ingressgateway.enabled=false \
+  --set gateways.istio-egressgateway.enabled=false \
+  --set gateways.istio-ilbgateway.enabled=false \
+  install/kubernetes/helm/istio \
+  -f install/kubernetes/helm/istio/example-values/values-istio-gateways.yaml \
+  | sed -e "s/custom-gateway/cluster-local-gateway/g" -e "s/customgateway/clusterlocalgateway/g" \
+  > ./istio-local-gateway.yaml
+
+kubectl apply -f istio-local-gateway.yaml
+
+kubectl apply --selector knative.dev/crd-install=true \
+   --filename https://github.com/knative/serving/releases/download/v0.9.0/serving.yaml \
+   --filename https://github.com/knative/eventing/releases/download/v0.9.0/release.yaml \
+   --filename https://github.com/knative/serving/releases/download/v0.9.0/monitoring.yaml
+
+
+kubectl apply --filename https://github.com/knative/serving/releases/download/v0.9.0/serving.yaml \
+   --filename https://github.com/knative/eventing/releases/download/v0.9.0/release.yaml \
+   --filename https://github.com/knative/serving/releases/download/v0.9.0/monitoring.yaml
+
 EOF
+
+}
+
 
 ####
 ####
