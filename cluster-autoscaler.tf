78297f3e896a98b1a3a29bd6fdcd793f3fab6a29
Ciaran Kelly <ciaran.kelly@ammeon.com>
2019-10-24 11:14:24 +0100

    Automate cluster autoscaling

---
+++ cluster-autoscaler.tf
@@ -0,0 +1,122 @@
+provider "kubernetes" {
+  config_path = module.eks.kubeconfig_filename
+}
+
+provider "helm" {
+  version = "~> 0.10"
+  max_history = 200
+  service_account = "tiller"
+  kubernetes {
+    config_path = module.eks.kubeconfig_filename
+  }
+}
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  automount_service_account_token = true
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "tiller-binding"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "tiller"
+    namespace = "kube-system"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "helm_release" "cluster_autoscaler" {
+  name      = "cluster-autoscaler"
+  chart     = "stable/cluster-autoscaler"
+  namespace = "kube-system"
+
+  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
+
+  set {
+    name  = "rbac.create"
+    value = true
+  }
+
+  set {
+    name  = "cloudprovider"
+    value = "aws"
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.region
+  }
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = local.cluster_name
+  }
+
+  set {
+    name  = "autoDiscovery.enabled"
+    value = true
+  }
+
+  set {
+    name  = "nodeSelector.kubernetes\\.io/lifecycle"
+    value = "spot"
+  }
+
+  set {
+    name  = "extraArgs.skip-nodes-with-system-pods"
+    value = false
+  }
+
+  set {
+    name  = "extraArgs.skip-nodes-with-local-storage"
+    value = false
+  }
+
+  set {
+    name  = "extraArgs.scale-down-unneeded-time"
+    value = "2m"
+  }
+
+  set {
+    name  = "extraArgs.scale-down-delay-after-add"
+    value = "2m"
+  }
+
+  set {
+    name  = "extraArgs.scale-down-unready-time"
+    value = "2m"
+  }
+
+}
+
+resource "helm_release" "spot_termination_handler" {
+  name = "spot-termination-handler"
+  chart = "stable/k8s-spot-termination-handler"
+  namespace = "kube-system"
+  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
+}
+
+resource "helm_release" "spot_rescheduler" {
+  name = "spot-rescheduler"
+  chart = "stable/k8s-spot-rescheduler"
+  namespace = "kube-system"
+  depends_on = ["kubernetes_service_account.tiller", "kubernetes_cluster_role_binding.tiller"]
+
+  set {
+    name  = "rbac.create"
+    value = true
+  }
+}
--- templates/postdeploy.tpl
+++ templates/postdeploy.tpl
@@ -5,16 +5,7 @@ Next Steps:
     KUBECONFIG=~/.kube/config:kubeconfig_${cluster_name} kubectl config view --flatten > ~/.kube/mergedkub && mv ~/.kube/mergedkub ~/.kube/config
     kubectl config use-context eks_${cluster_name}
 
- 2. Install and init helm / tiller:
-    kubectl create -f k8s/helm-rbac-config.yaml
-    helm init --service-account tiller --history-max 200
-
- 3. Activate cluster-autoscaling:
-    helm install stable/cluster-autoscaler --name cluster-autoscaler --namespace kube-system --values=./k8s/cluster-autoscaler-values.yaml
-    helm install stable/k8s-spot-termination-handler --name spot-termination-handler --namespace kube-system
-    helm install stable/k8s-spot-rescheduler --name spot-rescheduler --set rbac.create=true --namespace kube-system
-
- 4. Install knative
+ 2. Install knative
     helm repo add tm https://storage.googleapis.com/triggermesh-charts
     helm repo update
     helm install tm/knative --name knative
